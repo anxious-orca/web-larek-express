0e4566b6d27ad5ba71beef5bab7cdfc176525c46
anxious-orca <whatupmyglipglops@yandex.ru>
2026-02-16 10:37:36 +0300

    feat: добавил логгер и config файл

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,25 +2,29 @@ import express from 'express'
 import mongoose from 'mongoose';
 import path from 'path';
 import { errors } from 'celebrate';
+import { config } from './config';
 import router from './routes/index';
+import { requestLogger, errorLogger } from './middlewares/logger';
 import errorHandler from './middlewares/error-handler';
-
-const { PORT = 3000 } = process.env;
 
 const cors = require('cors');
 const app = express();
 
+app.use(requestLogger);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/weblarek');
+mongoose.connect(config.dbAdress);
 
 app.use(cors());
 app.use('/', router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT} ;)`)
+app.listen(config.port, () => {
+    console.log(`listening on port ${config.port} ;)`)
 });
--- backend/src/middlewares/error-handler.ts
+++ backend/src/middlewares/error-handler.ts
@@ -7,23 +7,18 @@ const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.error(err);
-
-  // Handle known operational errors
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({
       message: err.message
     });
   }
 
-  // Handle Mongoose validation errors
   if (err instanceof Error && err.name === 'ValidationError') {
     return res.status(400).json({
       message: err.message
     });
   }
 
-  // Fallback — unknown error
   return res.status(500).json({
     message: 'Internal server error'
   });
